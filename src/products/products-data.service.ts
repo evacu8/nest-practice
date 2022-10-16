78e74213bf222df585b790c825fe40609d5dada9
Krzysztof Potocki <kpotocki89@gmail.com>
2022-10-16 18:15:47 +0200

    Add products service and controller methods

--- src/products/products-data.service.ts
+++ src/products/products-data.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './interfaces/product.interface';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class ProductsDataService {}
+export class ProductsDataService {
+  private products: Array<Product> = [];
+
+  addProduct(_item_: CreateProductDto): Product {
+    const product: Product = {
+      ..._item_,
+      id: uuidv4(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    this.products.push(product);
+    return product;
+  }
+
+  deleteProduct(id: string): void {
+    this.products = this.products.filter((i) => i.id !== id);
+  }
+
+  updateProduct(id: string, dto: UpdateProductDto): Product {
+    const product = this.getProductById(id);
+    const index = this.products.findIndex((item) => item.id === id);
+    this.products[index] = { ...product, ...dto, updatedAt: new Date() };
+    return this.products[index];
+  }
+
+  getProductById(id: string): Product {
+    return this.products.find((product) => product.id === id);
+  }
+
+  getAllProducts(): Array<Product> {
+    return this.products;
+  }
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,16 +1,64 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ExternalProductDto } from './dto/external-product.dto';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsDataService } from './products-data.service';
+import { Product } from './interfaces/product.interface';
+import { dateToArray } from 'src/helpers/date.helper';
 
 @Controller('products')
 export class ProductsController {
-  @Get(':id')
-  getProductById(@Param('id') _id_: string): string {
-    return `GetByID ${_id_}`;
-  }
+  // eslint-disable-next-line prettier/prettier
+  constructor(private productsRepository: ProductsDataService) { }
 
   @Post()
-  addIProduct(@Body() _product_: CreateProductDto): ExternalProductDto {
-    return null;
+  addProduct(@Body() item: CreateProductDto): ExternalProductDto {
+    return this.mapProductToExternal(this.productsRepository.addProduct(item));
+  }
+
+  @Get()
+  getAllProducts(): ExternalProductDto[] {
+    const products = this.productsRepository.getAllProducts();
+    return products.map((p) => this.mapProductToExternal(p));
+  }
+
+  @Get(':id')
+  getProductById(@Param('id') _id_: string): ExternalProductDto {
+    return this.mapProductToExternal(
+      this.productsRepository.getProductById(_id_),
+    );
+  }
+
+  @Put(':id')
+  updateProduct(
+    @Param('id') _id_: string,
+    @Body() _product_: UpdateProductDto,
+  ): ExternalProductDto {
+    return this.mapProductToExternal(
+      this.productsRepository.updateProduct(_id_, _product_),
+    );
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  deleteProduct(@Param('id') _id_: string): void {
+    this.productsRepository.deleteProduct(_id_);
+  }
+
+  mapProductToExternal(product: Product): ExternalProductDto {
+    return {
+      ...product,
+      createdAt: dateToArray(product.createdAt),
+      updatedAt: dateToArray(product.updatedAt),
+    };
   }
 }
